e5d8c5c2165b9855b06f9e0e16790b7d3e12a6c0
jtmp2r <jtmp2r@gmail.com>
2015-08-26 19:58:10 -0500

    bread commit

---
+++ bread.js
@@ -0,0 +1,20 @@
+var Sandwich = (function(bread) {
+  var breadOption = {
+    "wheat": "2.00",
+    "rye": "1.50",
+    "pumpernickel": ".75"
+    "cranberry pecan": "6.00"
+    "white": "1.00"
+  }
+
+  sandwich.prototype.getBread = function(type){
+    var breadMan = type.split();
+    var pickBread = [];
+    for (var t = 0; t < hungryMan.length; t++) {
+      pickBread.push(breadOption[breadMan[t]];
+    }
+
+  }
+
+ return bread;
+}(Sandwich));

65959ac8f1c9d879e49ce9e2fdbd3673dd3c217d
Sequina Caro <sccaro89@gmail.com>
2015-08-26 20:03:18 -0500

    adding sandwich objects

---
+++ toppings.js
@@ -0,0 +1,35 @@
+var Sandwich = (function () {
+	var cheeseOpts = {
+		"provolone":"1.00"
+		"american":"1.50"
+		"swiss":"2.00"	
+
+	},
+
+	var meatOpts = {
+		"turkey":"2.00"
+		"chicken":"2.50"
+		"veggie":"3.00"
+	}
+
+
+	};
+
+	return {
+		getcheeseOpts: function() {
+			return cheeseOpts;
+		},
+
+		getmeatOpts: function() {
+			return meatOpts;
+		}
+
+
+		
+				
+	} 
+
+
+
+
+})
